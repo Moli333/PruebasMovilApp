18b007e8e5ef3aba552c953088d84898860a0416
Mónica Atehortúa <105534013+Moli333@users.noreply.github.com>
2023-05-02 09:09:35 -0500

    Entrega Reac N, API Colombia

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>El sentido de la vida es tener valores, no cosas de valor.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#800080 ',
     alignItems: 'center',
     justifyContent: 'center',
   },
